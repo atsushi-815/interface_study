10ab4f31e31086919aeb7e16c8ddb99daf02221c
atsushi-815 <atsushi586@yahoo.co.jp>
2019-08-03 23:37:12 +0900

    コメント追加

--- Battery.php
+++ Battery.php
@@ -3,11 +3,13 @@
 //interfaceにはfunction名、引数、返り値しか記載しない。（引数、返り値は場合によりけり）
 interface Battery
 {
-    //引数int型で取得、返り値もint型指定
+    //引数int型で取得し変数名minutesに格納、返り値もint型指定
     public function charge(int $minutes): int;
 }
 
 //interfaceの実装には「extends」ではなく「implements」というキーワードを使用
+
+//chargeの時間*5だけ歩くクラスを実装
 class GoodBattery implements Battery
 {
     public function charge(int $minutes): int
@@ -16,7 +18,7 @@ class GoodBattery implements Battery
     }
 }
 
-
+//chargeの時間*1だけ歩くクラスを実装
 class BadBattery implements Battery
 {
     public function charge(int $minutes): int
@@ -27,11 +29,12 @@ class BadBattery implements Battery
 
 class Robot
 {
-    //protectedはそのクラス自身と継承クラスからアクセス可能
+    //protectedはそのクラス自身と継承クラスからアクセス可能。
     protected $battery;
     protected $power = 0;
 
     //メソッド名の部分を__constructとすると、それがコンストラクタとなる
+    //battery関数の初期化を行なっている
     public function __construct(Battery $battery)
     {
         $this->battery = $battery;
@@ -54,7 +57,9 @@ class Robot
     }
 }
 
+//BadBatteryのクラスからインスタンスを作成し、変数名をbad_batteryで定義
 $bad_battery = new BadBattery;
+//BadBatteryを積んだロボットを作成
 $robot = new Robot($bad_battery);
 $robot->charge(10);
 $robot->walk();
